d5a38c8adcad8e45023f5d7910bdab86d17c6ef1
L_Murray <zaze1325@gmail.com>
2024-05-20 15:42:55 -0400

    setting up framework for templating

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates")
 
 @app.route("/")
 def index():
-    return "testcase"
+    """Root"""
+    return render_template(template_name_or_list="index.html", myvalue="regex101")
 
 
-app.run(host="0.0.0.0", port=80)
+if __name__ == "__main__":
+
+    app.run(host="0.0.0.0", port=80, debug=True)
